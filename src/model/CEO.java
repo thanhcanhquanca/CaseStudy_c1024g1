5944e07d5dca484dc339006c77fa2f69cf2d180c
thanhcanhquanca <chuanhquan89@gmail.com>
2025-01-07 15:04:37 +0700

    ceomodel

--- src/model/CEO.java
+++ src/model/CEO.java
@@ -1,4 +1,19 @@
 package model;
 
 public class CEO extends Person{
+    private String position;
+
+    public CEO(String id, String name, int age, String email, String phoneNumber, String department, String position) {
+        super(id, name, age, email, phoneNumber, department);
+        this.position = position;
+
+    }
+
+    public String getPosition() {
+        return position;
+    }
+
+    public void setPosition(String position) {
+        this.position = position;
+    }
 }

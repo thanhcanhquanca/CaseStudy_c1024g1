787626220d5de90d72ec2b6caa5586cd6d7862b3
thanhcanhquanca <chuanhquan89@gmail.com>
2025-01-07 15:07:23 +0700

    employeeModel

---
+++ src/model/Employee.java
@@ -0,0 +1,18 @@
+package model;
+
+public class Employee extends Person{
+    private String role;
+
+    public Employee(String id, String name, int age, String email, String phoneNumber, String department, String role) {
+        super(id, name, age, email, phoneNumber, department);
+        this.role = role;
+    }
+
+    public String getRole() {
+        return role;
+    }
+
+    public void setRole(String role) {
+        this.role = role;
+    }
+}

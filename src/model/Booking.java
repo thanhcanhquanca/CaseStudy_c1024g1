6b0cdc84b5d1570a55831de9ed4072fc258ed3b8
thanhcanhquanca <chuanhquan89@gmail.com>
2025-01-07 15:36:31 +0700

    bookingModel

---
+++ src/model/Booking.java
@@ -0,0 +1,70 @@
+package model;
+
+import java.time.LocalDate;
+
+public class Booking {
+    private String idBooking;
+    private String customerID;
+    private String roomId;
+    private LocalDate checkInDate;
+    private LocalDate checkOutDate;
+    private double totalPrice;
+
+
+    public Booking(String idBooking, String customerID, String roomId, LocalDate checkInDate, LocalDate checkOutDate, double totalPrice) {
+        this.idBooking = idBooking;
+        this.customerID = customerID;
+        this.roomId = roomId;
+        this.checkInDate = checkInDate;
+        this.checkOutDate = checkOutDate;
+        this.totalPrice = totalPrice;
+    }
+
+    public String getIdBooking() {
+        return idBooking;
+    }
+
+    public void setIdBooking(String idBooking) {
+        this.idBooking = idBooking;
+    }
+
+    public String getCustomerID() {
+        return customerID;
+    }
+
+    public void setCustomerID(String customerID) {
+        this.customerID = customerID;
+    }
+
+    public String getRoomId() {
+        return roomId;
+    }
+
+    public void setRoomId(String roomId) {
+        this.roomId = roomId;
+    }
+
+    public LocalDate getCheckInDate() {
+        return checkInDate;
+    }
+
+    public void setCheckInDate(LocalDate checkInDate) {
+        this.checkInDate = checkInDate;
+    }
+
+    public LocalDate getCheckOutDate() {
+        return checkOutDate;
+    }
+
+    public void setCheckOutDate(LocalDate checkOutDate) {
+        this.checkOutDate = checkOutDate;
+    }
+
+    public double getTotalPrice() {
+        return totalPrice;
+    }
+
+    public void setTotalPrice(double totalPrice) {
+        this.totalPrice = totalPrice;
+    }
+}
